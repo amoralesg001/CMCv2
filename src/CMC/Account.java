83e99e1627e97197f8bd8e8a93fb7b5945f6dee2
AMORALESG001 <amoralesg001@vplincsci030.ad.csbsju.edu>
2020-02-26 11:00:23 -0600

    adding onto Account class

--- src/CMC/Account.java
+++ src/CMC/Account.java
@@ -8,15 +8,17 @@
  */
 package CMC;
 public class Account {
+	private DBController dbController = new DBController();
+	
 	private String username;
 	private String password;
 	private String firstName;
 	private String lastName;
 	private String userType;
-	private boolean loginStatus;
+	private boolean loginStatus;// would we need this?
 	
 	public String getUsername() {
-		return this.username;
+		return this.username; 
 	}
 	
 	public String getPassword() {
@@ -38,7 +40,15 @@ public class Account {
 		
 	}
 	public boolean verify(String username, String password) {
+		if (this.username == username && this.password == password)
+		{
+		 
+		return true;
+		}
+		else {
 		return false;
+		}
+			
 	}
 	public void updateAccountInfo(String userName, String password, String fName, String lName) {
 		
