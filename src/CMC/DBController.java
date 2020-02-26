2c7f1d2fbc684769a8b74faaa25fd9b22bf0432f
AMORALESG001 <amoralesg001@vplincsci028.ad.csbsju.edu>
2020-02-25 23:30:35 -0600

    began writing the methods and instances for DBController

--- src/CMC/DBController.java
+++ src/CMC/DBController.java
@@ -1,42 +1,59 @@
-package CMC;
-public class DBController {
-	private Account user; 
-	
-	public Account getAccountDB(String name) {
-		return user; 
-	}
-	
-	public void getSavedSchools() {
+	package CMC;
+	public class DBController {
+		private Account user; 
+		private School university; 
 		
-	}
-	
-	public void getSchool() {
+		public Account getAccountDB(String name) {
+			return user; 
+		}
+		public School getUniversityDB(String name) {
+			return university; 
+		}
+		public void updateUniversity(School schoolName) {
+			
+		}
+		public void updateBlackListDB(School schoolName) {
+			
+		}
+		public void searchSchools(String schooName) {
+			
+		}
+		public void addSchool(String schoolName) {//is this adding all information of a school or just on the saved list? 
 		
-	}
-	
-	public void getUserInfo(String username) {
+		}
+		public void updateSavedSchools() {
 		
-	}
-	
-	public void searchSchools(String schooName) {
+		}
+		public void getSavedSchools() {
+			
+		}
 		
-	}
-	
-	public void updateSavedSchools() {
+		public void getAllSchools() {	//how to return an array 
+			
+		}
+		public void getAllUsers() {		//how to return an array 
+			
+		}
 		
-	}
-	
-	public void updateProfile() {
+		public void updateAccountDB(Account user) {
+			
+		}
 		
-	}
-	
-	public void login(String username, String password) {
+		public Account login(String username, String password) {
+			return user;
+		}
 		
+		public void removeSchool(String schoolName) {
+			
+		}
+		public School lookUpSchools(String schoolName) {
+			return university ;
+		}
+		public Account lookUpAccount(String accountName) {
+			return user; 
+		}
+		public void updateProfile() {
+			
+		}
 	}
 	
-	public void removeSchool(String schoolName) {
-		
-	}
-	
-}
-
