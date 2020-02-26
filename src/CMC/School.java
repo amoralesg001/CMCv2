7d7e4109e0c0bf6ccf33ce08c42742f0f3b6a2ba
JENGH001 <61020582+bluebous@users.noreply.github.comn>
2020-02-25 22:28:49 -0600

    Added getters and setters to School.java

--- src/CMC/School.java
+++ src/CMC/School.java
@@ -1,4 +1,5 @@
 package CMC;
+import java.util.ArrayList;
 import java.util.List;
 
 public class School {
@@ -18,6 +19,198 @@ public class School {
 	private int academicScale;
 	private int socialScale;
 	private int qoaSCale;
-	private List emphasis;
+	private ArrayList<String> emphasis;
+	/**
+	 * @return the schoolName
+	 */
+	public String getSchoolName() {
+		return schoolName;
+	}
+	/**
+	 * @param schoolName the schoolName to set
+	 */
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	/**
+	 * @return the numStudents
+	 */
+	public Long getNumStudents() {
+		return numStudents;
+	}
+	/**
+	 * @param numStudents the numStudents to set
+	 */
+	public void setNumStudents(Long numStudents) {
+		this.numStudents = numStudents;
+	}
+	/**
+	 * @return the femalePer
+	 */
+	public int getFemalePer() {
+		return femalePer;
+	}
+	/**
+	 * @param femalePer the femalePer to set
+	 */
+	public void setFemalePer(int femalePer) {
+		this.femalePer = femalePer;
+	}
+	/**
+	 * @return the verSAT
+	 */
+	public int getVerSAT() {
+		return verSAT;
+	}
+	/**
+	 * @param verSAT the verSAT to set
+	 */
+	public void setVerSAT(int verSAT) {
+		this.verSAT = verSAT;
+	}
+	/**
+	 * @return the mathSAT
+	 */
+	public int getMathSAT() {
+		return mathSAT;
+	}
+	/**
+	 * @param mathSAT the mathSAT to set
+	 */
+	public void setMathSAT(int mathSAT) {
+		this.mathSAT = mathSAT;
+	}
+	/**
+	 * @return the tuition
+	 */
+	public double getTuition() {
+		return tuition;
+	}
+	/**
+	 * @param tuition the tuition to set
+	 */
+	public void setTuition(double tuition) {
+		this.tuition = tuition;
+	}
+	/**
+	 * @return the finAid
+	 */
+	public double getFinAid() {
+		return finAid;
+	}
+	/**
+	 * @param finAid the finAid to set
+	 */
+	public void setFinAid(double finAid) {
+		this.finAid = finAid;
+	}
+	/**
+	 * @return the numApplicants
+	 */
+	public double getNumApplicants() {
+		return numApplicants;
+	}
+	/**
+	 * @param numApplicants the numApplicants to set
+	 */
+	public void setNumApplicants(double numApplicants) {
+		this.numApplicants = numApplicants;
+	}
+	/**
+	 * @return the admitPer
+	 */
+	public int getAdmitPer() {
+		return admitPer;
+	}
+	/**
+	 * @param admitPer the admitPer to set
+	 */
+	public void setAdmitPer(int admitPer) {
+		this.admitPer = admitPer;
+	}
+	/**
+	 * @return the enrolledPer
+	 */
+	public int getEnrolledPer() {
+		return enrolledPer;
+	}
+	/**
+	 * @param enrolledPer the enrolledPer to set
+	 */
+	public void setEnrolledPer(int enrolledPer) {
+		this.enrolledPer = enrolledPer;
+	}
+	/**
+	 * @return the academicScale
+	 */
+	public int getAcademicScale() {
+		return academicScale;
+	}
+	/**
+	 * @param academicScale the academicScale to set
+	 */
+	public void setAcademicScale(int academicScale) {
+		this.academicScale = academicScale;
+	}
+	/**
+	 * @return the socialScale
+	 */
+	public int getSocialScale() {
+		return socialScale;
+	}
+	/**
+	 * @param socialScale the socialScale to set
+	 */
+	public void setSocialScale(int socialScale) {
+		this.socialScale = socialScale;
+	}
+	/**
+	 * @return the qoaSCale
+	 */
+	public int getQoaSCale() {
+		return qoaSCale;
+	}
+	/**
+	 * @param qoaSCale the qoaSCale to set
+	 */
+	public void setQoaSCale(int qoaSCale) {
+		this.qoaSCale = qoaSCale;
+	}
+	/**
+	 * @return the emphasis
+	 */
+	public ArrayList<String> getEmphasis() {
+		return emphasis;
+	}
+	/**
+	 * @param emphasis the emphasis to set
+	 */
+	public void setEmphasis(ArrayList<String> emphasis) {
+		this.emphasis = emphasis;
+	}
 	
 }
