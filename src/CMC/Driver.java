a8ba042c5cd707a4d52921b7ab80b1cf03f518b9
NLARSON002 <nlarson002@vplincsci075.ad.csbsju.edu>
2020-02-26 23:36:25 -0600

    plz github

--- src/CMC/Driver.java
+++ src/CMC/Driver.java
@@ -18,16 +18,11 @@ public class Driver {
 	private UserUI userUI = new UserUI();
 	private DBController dbController = new DBController();
 	private Account account = new Account();
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	/**
 	 * U1: LogIn
 	 */
 
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System. in);
 		System.out.println("test");
 		System.out.println("Enter a school you want to remove");
@@ -36,15 +31,10 @@ public class Driver {
 	}
 
 	public void u1() {
-<<<<<<< HEAD
-		private String u = "";
-		private String pw = "";
+		String u = "";
+		String pw = "";
 		userUI.login(u, pw);
-=======
-		userUI.logIn(String x, String y);
-		userControl.verify();
-		userControl.getAccount();
->>>>>>> 1bb4d336c72c6989d642e5f426aeec19d6b83d98
+
 	}
 	
 	/**
