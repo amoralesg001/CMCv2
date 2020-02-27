4d910a73cfd612e138ed9b7c998a47474141e4ff
JYOUNG001 <jyoung001@vplincsci003.ad.csbsju.edu>
2020-02-26 19:06:24 -0600

    Jack: Changes made to the UniversityController and User classes.

--- src/CMC/UniversityController.java
+++ src/CMC/UniversityController.java
@@ -7,13 +7,74 @@
  *
  */
 package CMC;
+
+import java.util.ArrayList;
+
 public class UniversityController {
 	
 	private DBController dbController;
 	private School school;
 	
+	/**
+	 * Gets the university by name from the database.
+	 * @param schoolName
+	 */
 	public void getUniversity(String schoolName) {
 		this.dbController.getSchool(schoolName);
-		
 	}
+	
+	/**
+	 * Creates a new school and adds it to the database.
+	 * @param schoolName
+	 * @param state
+	 * @param location
+	 * @param numStudents
+	 * @param femalePer
+	 * @param verSAT
+	 * @param mathSAT
+	 * @param tuition
+	 * @param finAid
+	 * @param numApplicants
+	 * @param admitPer
+	 * @param enrolledPer
+	 * @param academicScale
+	 * @param socialScale
+	 * @param qoaSCale
+	 * @param emphasis
+	 */
+	public void addSchool(String schoolName, String state, String location, Long numStudents, int femalePer, int verSAT,
+			int mathSAT, double tuition, double finAid, double numApplicants, int admitPer, int enrolledPer,
+			int academicScale, int socialScale, int qoaSCale, ArrayList<String> emphasis) {
+		// TODO
+	}
+	
+	/**
+	 * Removes a school from the database by name.
+	 * @param schoolName
+	 */
+	public void removeSchool(String schoolName) {
+		this.dbController.removeSchool(schoolName);
+	}
+	
+	/**
+	 * Edits the university information. 
+	 */
+	public void editUniversityInfo() {
+		// TODO
+	}
+	
+	/**
+	 * Gets a list of all the universities.
+	 */
+	public void getAllUniversities() {
+		// TODO
+	}
+	
+	/**
+	 * Adds the university to the blacklist.
+	 */
+	public void blacklistUniversity() {
+		// TODO
+	}
+	
 }
