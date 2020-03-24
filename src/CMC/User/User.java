035cb7312c651553e716c8377fb863c7822ce483
John Engh <14243398+Frogdart5@users.noreply.github.com>
2020-03-24 13:25:17 -0500

    changed user to be a subclass of Account and updated method calls accordingly

--- src/CMC/User/User.java
+++ src/CMC/User/User.java
@@ -8,26 +8,24 @@
  */
 package CMC.User;
 public class User extends Account{
-	
-	private Account account = new Account();
 
 	public User(String username, String password, String firstName, String lastName, String status) {
 		super(username, password, firstName, lastName, "u", status);
 	}
 	
 	public void setUserName(String name) {
-		this.account.updateAccountInfo(name, this.account.getPassword(), this.account.getFirstName(), this.account.getLastName(), this.account.getUserType(), this.account.getLoginStatus());
+		super.updateAccountInfo(name, super.getPassword(), super.getFirstName(), super.getLastName(), super.getUserType());
 	}
 	
 	public void setUserPassword(String password) {
-		this.account.updateAccountInfo(this.account.getUsername(), password, this.account.getFirstName(), this.account.getLastName());
+		super.updateAccountInfo(super.getUsername(), password, super.getFirstName(), super.getLastName(), super.getUserType());
 	}
 	
 	public void setUserFirstName(String firstName) {
-		this.account.updateAccountInfo(this.account.getUsername(), this.account.getPassword(), firstName, this.account.getLastName());
+		super.updateAccountInfo(super.getUsername(), super.getPassword(), firstName, super.getLastName(), super.getUserType());
 	}
 	
 	public void setUserLastName(String lastName) {
-		this.account.updateAccountInfo(this.account.getUsername(), this.account.getPassword(), this.account.getFirstName(), lastName);
+		super.updateAccountInfo(super.getUsername(), super.getPassword(), super.getFirstName(), lastName, super.getUserType());
 	}
 }
