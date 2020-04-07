99833178fa5f7322ab05eef32faffc1ee76406a5
RVANG001 <rvang001@vplincsci047.ad.csbsju.edu>
2020-04-07 16:00:37 -0500

    addding testcases

---
+++ test/CMC/University/UniversityControllerTest.java
@@ -0,0 +1,11 @@
+package CMC.University;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestAddUniversity.class, TestEditUniversity.class })
+public class UniversityControllerTest {
+
+}
