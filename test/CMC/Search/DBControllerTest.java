c4f556155e22305082a25e5e87bbac4ef899cc4a
NLARSON002 <nlarson002@vplincsci039.ad.csbsju.edu>
2020-04-08 16:25:55 -0500

    U1 Test Complete

--- test/CMC/Search/DBControllerTest.java
+++ test/CMC/Search/DBControllerTest.java
@@ -75,4 +75,26 @@ public class DBControllerTest {
 		Assert.assertTrue("Saved university to list", updateSavedUniversities(username, uniName))
 	}
 
+	
+	
+	
+	
+	//Testing U1: Login
+	//Nick
+	@Test
+	public void testGetAccountDB() {
+		String username = "juser";
+		String password = "user";
+		
+		String wrongUsername = "juserr"
+		String wrongPassword = "userr"
+		
+		Account testAccount  = DBController.getAccountDB(username, password);
+		Account testWrongUsername = DBController.getAccountDB(wrongUsername, password);
+		Account testWrongPassword = DBController.getAccountDB(username, wrongPassword);
+		
+		Assert.assertTrue((testAccount.getUsername() == juser) && testAccount.getPassword() == password);
+		Assert.assertTrue(testWrongUsername.getUsername() == null);
+		Assert.assertTrue(testWrongPassword.getPassword() == null);
+	}
 }
