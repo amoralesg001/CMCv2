699ad66420b85da32f36b5f1942d3e38d494c78f
NLARSON002 <nlarson002@vplincsci037.ad.csbsju.edu>
2020-04-14 18:26:34 -0500

    idk what i changed

--- test/CMC/User/UserControllerTest.java
+++ test/CMC/User/UserControllerTest.java
@@ -9,13 +9,25 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class UserControllerTest {
-
+	public Account validAccount;
+	public String validUsername;
+	public String password;
 	@Before
 	public void setUp() throws Exception {
+		String validUsername = "username";
+		String password = "password";
+		String firstName = "First Name";
+		String lastName = "Last Name";
+		String userType = "User Type";
+		String loginStatus = "Login Status";
+		//String invalidUsername = null;
+		Account validAccount = new Account(validUsername, password, firstName, lastName, userType, loginStatus);
+		Account invalidAccount = new Account(null, null, null, null, null, null);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		validAccount = null;
 	}
 
 	// Testing U7: Save User Info
@@ -27,16 +39,6 @@ public class UserControllerTest {
 	
 	@Test 
 	public void testGetUserInfo() {
-		String validUsername = "username";
-		String password = "password";
-		String firstName = "First Name";
-		String lastName = "Last Name";
-		String userType = "User Type";
-		String loginStatus = "Login Status";
-		//String invalidUsername = null;
-		Account validAccount = new Account(validUsername, password, firstName, lastName, userType, loginStatus);
-		Account invalidAccount = new Account(null, null, null, null, null, null);
-		
 		Assert.assertTrue("Testing valid account", validAccount.getUserInfo(validUsername, password) != null);
 		
 	}
